d1a9b90cb224ebae4e91a28e3c6cf5ac8463f257
ysamuelII <yadavallisamuelii@gmail.com>
2023-06-26 15:43:40 +0530

    update

---
+++ src/main/App.java
@@ -0,0 +1,11 @@
+package com.mycompany;
+public class App  
+{   
+public static void main(String[] args)    
+{           
+System.out.print("Enter a string: ");   
+//reading a string from the user  
+String name = System.console().readLine();   
+System.out.println("You have entered: "+name);   
+}   
+}  
---
+++ src/test/App.java
@@ -0,0 +1,20 @@
+package com.mycompany;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class App
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
